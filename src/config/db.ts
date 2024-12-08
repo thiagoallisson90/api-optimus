3ad9abad8de41614c84f3f6276e2b4c0149f14bf
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-12-07 23:36:45 -0300

    refactor: add connection callbacks

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,25 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn: mongoose.Mongoose = await mongoose.connect(
-      process.env.MONGO_URI || ""
-    );
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    mongoose.connection.on("connected", () => {
+      if (process.env.NODE_ENV === "development") {
+        console.log(`MongoDB Connected`);
+      }
+    });
+
+    mongoose.connection.on("error", (error: any) => {
+      if (process.env.NODE_ENV === "development") {
+        console.error("Error on connection", error);
+      }
+    });
+
+    await mongoose.connect(process.env.MONGO_URI || "");
+
+    process.on("SIGINT", async () => {
+      await mongoose.connection.close();
+      console.log("Conection closed");
+      process.exit(0);
+    });
   } catch (error: any) {
     if (process.env.NODE_ENV === "development") {
       console.error(`Error: ${error}`);
